1726d6e81ed79805e6091a625bb55c8aaffe55b1
zerokingmaker <85169323+zerokingmaker@users.noreply.github.com>
2023-01-11 13:17:18 +0000

    Create script.py

---
+++ script.py
@@ -0,0 +1,27 @@
+import os
+from docx import Document
+
+def replace_text_in_docx(file_path, old_text, new_text):
+    document = Document(file_path)
+    for p in document.paragraphs:
+        if old_text in p.text:
+            inline = p.runs
+            for i in range(len(inline)):
+                if old_text in inline[i].text:
+                    text = inline[i].text.replace(old_text, new_text)
+                    inline[i].text = text
+    document.save(file_path)
+    
+def search_and_replace(root_dir, old_text, new_text):
+    for dirpath, dirnames, filenames in os.walk(root_dir):
+        for filename in filenames:
+            if filename.endswith('.docx'):
+                file_path = os.path.join(dirpath, filename)
+                replace_text_in_docx(file_path, old_text, new_text)
+
+root_dir =  r'c://path'
+old_text = 'word1'
+new_text = 'word2'
+search_and_replace(root_dir, old_text, new_text)
+
+print("All done!")
